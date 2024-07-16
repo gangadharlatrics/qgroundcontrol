459e7180f34ed08e93ed31c3079d71f8dac392a1
gangadharlatrics <gangadhar.vasa@latrics.com>
2024-07-16 15:44:33 +0530

    change app name
    change app name

--- cmake/CustomOptions.cmake
+++ cmake/CustomOptions.cmake
@@ -1,6 +1,6 @@
 # The following options can be overriden by custom builds using the CustomOverrides.cmake file
-set(QGC_APP_NAME "QGroundControl")
-set(QGC_APP_COPYRIGHT "Copyright (c) 2024 QGroundControl. All rights reserved.")
+set(QGC_APP_NAME "KryoasControlStation")
+set(QGC_APP_COPYRIGHT "Copyright (c) 2024 KryoasControlStation. All rights reserved.")
 set(QGC_APP_DESCRIPTION "Open Source Ground Control App")
 set(QGC_ORG_NAME "QGroundControl.org")
 set(QGC_ORG_DOMAIN "org.qgroundcontrol")
